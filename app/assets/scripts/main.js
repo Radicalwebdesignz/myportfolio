53b0938a6a234195f2d0964415a5fba4a6562a7f
Manohar <manohar.webdev@gmail.com>
2018-03-16 18:48:34 +0530

    Completed About and Skills Section

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+
+	// Nav link underline on hover & navbar bg color
 	$('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
 		$(".navbar").toggleClass('bg-red--nav');
 	});
 
+	// Fade banner content on scroll
 	$(window).scroll(function() {
 		if ($(window).scrollTop() > 100) {
 			$(".animate-one").css({
@@ -55,4 +58,54 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function() {
 
+  var curPage = 1;
+  var numOfPages = $(".skw-page").length;
+  var animTime = 1000;
+  var scrolling = false;
+  var pgPrefix = ".skw-page-";
+
+  function pagination() {
+    scrolling = true;
+
+    $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+    $(pgPrefix + (curPage - 1)).addClass("inactive");
+    $(pgPrefix + (curPage + 1)).removeClass("active");
+
+    setTimeout(function() {
+      scrolling = false;
+    }, animTime);
+  };
+
+  function navigateUp() {
+    if (curPage === 1) return;
+    curPage--;
+    pagination();
+  };
+
+  function navigateDown() {
+    if (curPage === numOfPages) return;
+    curPage++;
+    pagination();
+  };
+
+  $(document).on("mousewheel DOMMouseScroll", function(e) {
+    if (scrolling) return;
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+      navigateUp();
+    } else { 
+      navigateDown();
+    }
+  });
+
+  $(document).on("keydown", function(e) {
+    if (scrolling) return;
+    if (e.which === 38) {
+      navigateUp();
+    } else if (e.which === 40) {
+      navigateDown();
+    }
+  });
+
+});
